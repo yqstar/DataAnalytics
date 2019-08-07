e697d6964196efaf02f7040c46e562a16a871e4c
AndrewYq <1656302851@qq.com>
2019-08-07 10:09:43 +0800

    Update Bayes.py

--- Bayes.py
+++ Bayes.py
@@ -18,47 +18,47 @@ P(w|c), 在用户想键入 c 的情况下敲成 w 的概率. 因为这个是代�
 argmaxc, 用来枚举所有可能的 c 并且选取概率最大的
 '''
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# 把语料中的单词全部抽取出来, 转成小写, 并且去除单词中间的特殊符号
-def words(text):
+# 抽取语料中单词并转成小写,同时去除单词中特殊符号,反而言之可正则匹配出所有小写单词。
+def words(file_path):
+    text = open(file_path).read()
     return re.findall('[a-z]+', text.lower())
 
 
-'''
-要是遇到我们从来没有过见过的新词怎么办. 
-假如说一个词拼写完全正确, 但是语料库中没有包含这个词, 
-从而这个词也永远不会出现在训练集中. 
-于是, 我们就要返回出现这个词的概率是0. 
-这个情况不太妙, 因为概率为0这个代表了这个事件绝对不可能发生, 
-而在我们的概率模型中, 我们期望用一个很小的概率来代表这种情况. lambda: 1
-'''
-
-
+# 遇见新词处理办法，假如单词拼写完全正确, 但是语料库中没有包含该词，从而这个词也永远不会出现在训练集中。
+# 于是, 我们就要返回出现这个词的概率是0。 这个情况不太妙, 因为概率为0这个代表了这个事件绝对不可能发生。
+# 而在我们的概率模型中, 我们期望用一个很小的概率来代表这种情况. lambda: 1
+# 统计语料中各个单词的词频
 def train(features):
+    # defaultdict的作用是在于，当字典里的key不存在但被查找时，返回的不是keyError而是一个默认值
+    # dict =defaultdict(factory_function)，这个factory_function可以是list、set、str等等，作用是当key不存在时，返回的是工厂函数的默认值
+    # 比如list对应[]，str对应的是空字符串，set对应set()，int对应0，lambda:1对应1
     model = collections.defaultdict(lambda: 1)
     for f in features:
         model[f] += 1
     return model
 
+NWORDS = train(words('big.txt'))
 
-NWORDS = train(words(open('big.txt').read()))
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 '''
 编辑距离:
 两个词之间的编辑距离定义为使用了几次插入(在词中插入一个单字母),
 删除(删除一个单字母), 交换(交换相邻两个字母), 
-替换(把一个字母换成另一个)的操作从一个词变到另一个词.
+替换(把一个字母换成另一个)的操作从一个词变到另一个词。
 '''
 
+
 # 返回所有与单词 w 编辑距离为 1 的集合
+# 编辑距离计算
 def edits1(word):
     n = len(word)
-    return set([word[0:i]+word[i+1:] for i in range(n)] +                     # deletion
-               [word[0:i]+word[i+1]+word[i]+word[i+2:] for i in range(n-1)] + # transposition
-               [word[0:i]+c+word[i+1:] for i in range(n) for c in alphabet] + # alteration
-               [word[0:i]+c+word[i:] for i in range(n+1) for c in alphabet])  # insertion
+    return set([word[0:i]+word[i+1:] for i in range(n)] +                      # deletion
+               [word[0:i]+word[i+1]+word[i]+word[i+2:] for i in range(n-1)] +  # transposition
+               [word[0:i]+c+word[i+1:] for i in range(n) for c in alphabet] +  # alteration
+               [word[0:i]+c+word[i:] for i in range(n+1) for c in alphabet])   # insertion
+
 
 '''
 与 something 编辑距离为2的单词居然达到了 114,324 个
