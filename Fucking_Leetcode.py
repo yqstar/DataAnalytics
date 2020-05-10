4d69af6013ea700a92ac4ee136d1c4094f39f913
AndrewYq <1656302851@qq.com>
2020-05-10 23:24:19 +0800

    Update Fucking_Leetcode.py

--- Fucking_Leetcode.py
+++ Fucking_Leetcode.py
@@ -1,14 +1,24 @@
 class Solution:
     def bubble_sort(self, nums_list):
         n = len(nums_list)
-        for j in range(n-1):
-            for i in range(n-j-1):
+        for j in range(n - 1):
+            for i in range(n - j - 1):
                 if nums_list[i] > nums_list[i + 1]:
                     nums_list[i], nums_list[i + 1] = nums_list[i + 1], nums_list[i]
         return nums_list
 
+    def selection_sort(self, nums_list):
+        n = len(nums_list)
+        for i in range(n - 1):
+            min_idx = i
+            for j in range(i + 1, n - 1):
+                if nums_list[j] < nums_list[min_idx]:
+                    min_idx = j
+            nums_list[i], nums_list[min_idx] = nums_list[min_idx], nums_list[i]
+        return nums_list
 
 
 st = Solution()
 
-print(st.bubble_sort([1,9,5,4,10]))
+print(st.bubble_sort([1, 9, 5, 4, 10]))
+print(st.selection_sort([1, 9, 5, 4, 10]))
