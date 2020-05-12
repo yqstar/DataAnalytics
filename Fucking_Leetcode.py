80bc8bc3a2c993e586027b75c15687da803b6b6f
AndrewYq <1656302851@qq.com>
2020-05-12 12:32:59 +0800

    Update Fucking_Leetcode.py

--- Fucking_Leetcode.py
+++ Fucking_Leetcode.py
@@ -22,3 +22,23 @@ st = Solution()
 
 print(st.bubble_sort([1, 9, 5, 4, 10]))
 print(st.selection_sort([1, 9, 5, 4, 10]))
+
+class Queue(object):
+    """队列"""
+    def __init__(self):
+        self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def enqueue(self, item):
+        """进队列"""
+        self.items.insert(0,item)
+ 
+    def dequeue(self):
+        """出队列"""
+        return self.items.pop()
+ 
+    def size(self):
+        """返回大小"""
+        return len(self.items)
